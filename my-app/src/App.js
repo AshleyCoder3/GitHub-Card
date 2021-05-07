83dfc852e2b3d65e54f12f3d72396ad46fd4f669
Ashley Burns <Ash_burn999@yahoo.com>
2021-05-07 00:07:44 -0400

    "commented apis"

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Axios from 'axios'
 import './App.css';
 
+//https://api.github.com/users/<your name>
+//https://api.github.com/users/< Your github name >/followers
+
 class App extends React.Component {
+
+
   render() {
     return (
       <div className="App">
